ad839b55529362c84bdac67cbd0ee7a5661120ec
Martin Wilhelmi <martin.wilhelmi@akra.de>
2011-10-21 14:24:54 +0200

    remove warnings

--- Classes/MGSplitViewController.m
+++ Classes/MGSplitViewController.m
@@ -1001,7 +1001,7 @@
 - (UIViewController *)masterViewController
 {
 	if (_viewControllers && [_viewControllers count] > 0) {
-		NSObject *controller = [_viewControllers objectAtIndex:0];
+		UIViewController *controller = [_viewControllers objectAtIndex:0];
 		if ([controller isKindOfClass:[UIViewController class]]) {
 			return controller;
 		}
@@ -1043,7 +1043,7 @@
 - (UIViewController *)detailViewController
 {
 	if (_viewControllers && [_viewControllers count] > 1) {
-		NSObject *controller = [_viewControllers objectAtIndex:1];
+		UIViewController *controller = [_viewControllers objectAtIndex:1];
 		if ([controller isKindOfClass:[UIViewController class]]) {
 			return controller;
 		}
